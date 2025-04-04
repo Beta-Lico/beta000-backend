d5b72cfcbadb426c5caa1d5ab776a31644f718a5
Fernando Kendi <fedked@outlook.com>
2025-04-04 01:07:56 -0300

    feat: campos adicionados nas classes CriarProfissionalDto e Profissional

--- DTOs/CriarProfissionalDto.cs
+++ DTOs/CriarProfissionalDto.cs
@@ -3,6 +3,9 @@ namespace Plataforma.Backend_.DTOs;
 public class CriarProfissionalDto
 {
     public string Nome { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
     public string Telefone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string Localizacao { get; set; } = string.Empty;
+    public List<string> Servicos { get; set; } = new();
 }
--- Models/Profissional.cs
+++ Models/Profissional.cs
@@ -5,5 +5,8 @@ public class Profissional
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Nome { get; set; } = string.Empty;
     public string Telefone { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string Localizacao { get; set; } = string.Empty;
+    public List<string> Servicos { get; set; } = new();
 }
